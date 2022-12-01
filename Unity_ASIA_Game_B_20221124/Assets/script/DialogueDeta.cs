a34dba9260382d5d2fd8e72404b539316ab9ca6b
player200250 <113589716+player200250@users.noreply.github.com>
2022-12-01 18:41:34 +0800

    1201建立資料檔案

---
+++ Unity_ASIA_Game_B_20221124/Assets/script/DialogueDeta.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jay 
+{
+    /// <summery>
+    /// 對話資料
+    /// </summery>
+    [CreateAssetMenu(menuName = "Jay/Dialogue Deta", fileName = "New Dialogue Deta")]
+    public class DialogueDeta : ScriptableObject
+    {
+        [Header("對話者名稱")]
+        public string dialogueName;
+        [Header("對話者內容"), TextArea(2, 10)]
+        public string[] dialogueContents;
+    }
+}
+
