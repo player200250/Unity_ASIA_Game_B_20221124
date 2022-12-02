e2dc3927f827ab745da6bff6cc6b6b521cf233c6
player200250 <113589716+player200250@users.noreply.github.com>
2022-12-02 16:22:40 +0800

    1202建立對話

---
+++ Unity_ASIA_Game_B_20221124/Assets/script/DialogueSystem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace Jay 
+{
+    /// <summary>
+    /// 對話系統
+    /// </summary>
+    public class DialogueSystem : MonoBehaviour
+    {
+        #region 資料區域
+        [SerializeField, Header("對話間隔"), Range(0, 0.5f)]
+        private float dialogueIntervalTime = 0.1f;
+        [SerializeField, Header("開頭對話")]
+        private DialogueDeta dialogueOpening;
+
+        private WaitForSeconds dialogueInterval => new WaitForSeconds(dialogueIntervalTime);
+
+        private CanvasGroup groupDialogue;
+        private TextMeshProUGUI textName;
+        private TextMeshProUGUI textContext;
+        private GameObject goTriangle;
+        #endregion
+
+        #region 事件區域
+        private void Awake()
+        {
+            groupDialogue = GameObject.Find("對話系統").GetComponent<CanvasGroup>();
+            textName = GameObject.Find("對話者名稱").GetComponent<TextMeshProUGUI>();
+            textContext = GameObject.Find("對話內容").GetComponent<TextMeshProUGUI>();
+            goTriangle = GameObject.Find("對話圖示");
+            goTriangle.SetActive(false);
+
+        } 
+        #endregion
+    }
+
+}
+
