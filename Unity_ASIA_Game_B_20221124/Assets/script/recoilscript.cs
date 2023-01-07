dd9ceae0f8f78697c3dbd55f9c8c1455545676d4
player200250 <113589716+player200250@users.noreply.github.com>
2023-01-08 01:48:14 +0800

    0108更新

---
+++ Unity_ASIA_Game_B_20221124/Assets/script/recoilscript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MyNamespace
+{
+    /// <summary>
+    /// 射擊系統
+    /// </summary>
+    public class recoilscript : MonoBehaviour
+    {
+        public GameObject Gun;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                StartCoroutine(StartRecoil());
+            }
+        }
+
+        IEnumerator StartRecoil()
+        {
+            Gun.GetComponent<Animator>().Play("射擊動畫");
+            yield return new WaitForSeconds(0.20f);
+            Gun.GetComponent<Animator>().Play("New State");
+        }
+    }
+}
